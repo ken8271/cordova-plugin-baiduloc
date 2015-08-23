d6bef952f9e06353cae232008bbde877369175b4
ken <lubaoqin@xiangrikui.com>
2015-08-23 10:04:36 +0800

    修改代码

--- src/android/Baidulocation.java
+++ src/android/Baidulocation.java
@@ -4,6 +4,8 @@ package org.apache.cordova.plugin.baidulocation;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
